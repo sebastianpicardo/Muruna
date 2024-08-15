96574f9d10850df673c459e710c922dbbab1d489
sebastian picardo <spicardo@pgagroup.cl>
2024-08-15 18:31:07 -0400

    prueba sebastian picardo

---
+++ user-registration-api/user-registration-api/src/main/java/com/muruna/user_registration_api/UserRegistrationApiApplication.java
@@ -0,0 +1,14 @@
+package com.muruna.user_registration_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class UserRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationApiApplication.class, args);
+	}
+
+}
